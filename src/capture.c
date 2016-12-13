8bf44ca63cf17c971ecf378ff0b70d73a32e9b70
Jonathan Whiting <jonathan@ignika.com>
2016-12-13 14:02:25 +0000

    add some bits

---
+++ src/capture.c
@@ -0,0 +1,33 @@
+#include "capture.h"
+
+#include <stdio.h>
+
+#include <whitgl/input.h>
+#include <whitgl/logging.h>
+#include <whitgl/sys.h>
+
+capture_info capture_info_update(capture_info capture)
+{
+	if(whitgl_input_pressed(WHITGL_INPUT_Y))
+	{
+		capture.capturing = !capture.capturing;
+	}
+	if(!capture.capturing)
+	{
+		capture.frame = 0;
+		capture.timer = 0;
+		return capture;
+	}
+	if(capture.frame == 0)
+		capture.n++;
+	capture.timer--;
+	if(capture.timer > 0)
+		return capture;
+	capture.timer = 2;
+	char buffer[256];
+	snprintf(buffer, sizeof(buffer), "../../art/gifsrc/seq%04d_%04d.png", (int)capture.n, (int)capture.frame);
+	WHITGL_LOG("capturing %s", buffer);
+	whitgl_sys_capture_frame(buffer);
+	capture.frame++;
+	return capture;
+}
